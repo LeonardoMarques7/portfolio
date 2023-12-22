1ef10de7d29b44f56550fe492f3e0b06b8f96aa1
LeonardoMarques7 <leonardoemcs155@gmail.com>
2023-12-22 15:13:33 -0300

    adiciona script do nav

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const nav = `
+<ul class="nav-ul">
+    <li class="nav-item">
+        <a href="/" class="nav-link active">Home</a>
+    </li>
+    <li class="nav-item">
+        <a href="/sobre.html" class="nav-link">Sobre</a>
+    </li>
+    <li class="nav-item">
+        <a href="/projetos.html" class="nav-link">Projetos</a>
+    </li>
+    <li class="nav-item">
+        <a href="#" class="nav-link">Contato</a>
+    </li>
+</ul>
+`;
+
+function renderNav() {
+    const navEl = document.querySelector(".nav");
+    if (navEl) {
+        navEl.innerHTML = nav;
+
+        document.querySelectorAll(".nav-link").forEach((el) => {
+            const { pathname } = window.location;
+            if (pathname !== el.getAttribute("href")) {
+                return el.classList.remove("active");
+            }
+
+            return el.classList.add("active");
+        });
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    renderNav();
+});
